17c0080babce060f7981c67f8bc3818f98df1190
zzj-main <71962996+zzj-main@users.noreply.github.com>
2020-11-10 17:16:28 +0800

    修改test
    第一次使用GitHub修改文件，练习一把

--- test.java
+++ test.java
@@ -1,6 +1,7 @@
 public class test{
     public static void main(string arg[]){
-    System.out.print("hello world");
+        System.out.print("hello world1");
+        System.out.print("hello world2");
     }
 
 }
